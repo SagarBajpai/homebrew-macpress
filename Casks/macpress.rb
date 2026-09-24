d891478457ee450d43f5a504d2f3e12e2a8c9b6e
Sagar Bajpai <ashversache@gmail.com>
2026-09-24 11:54:36 +0530

    fix: use stable MacPress DMG URL

--- Casks/macpress.rb
+++ Casks/macpress.rb
@@ -2,7 +2,7 @@ cask "macpress" do
   version "0.1.0"
   sha256 "625f13bd10ad2c6fcf1c942a5a088f0ac785c578b3a4f54146482f245864b3d7"
 
-  url "https://github.com/SagarBajpai/MacPress/releases/download/v#{version}/MacPress-#{version}-arm64.dmg"
+  url "https://github.com/SagarBajpai/MacPress/releases/latest/download/MacPress-arm64.dmg"
   name "MacPress"
   desc "Automatically compress macOS screen recordings"
   homepage "https://github.com/SagarBajpai/MacPress"
